626bfbd79c790d5f619552a694a8e6bec0ff711c
Michael <michael.lazzaro112@gmail.com>
2016-04-05 20:03:28 -0400

    Refactor

--- main.controller.js
+++ main.controller.js
@@ -1,6 +1,33 @@
 angular.module('myApp')
 
 .controller('mainController', function($scope){
-  // $scope.test= "Hi";
 
+
+  $scope.first = true;
+  $scope.last = true;
+
+  $scope.toggle=function(){
+    if($scope.showme && $scope.tryagain){
+      $scope.showme=false;
+      $scope.tryagain=false;
+      $scope.firstname='';
+      $scope.lastname=''
+      $scope.first =true;
+      $scope.last =true;
+    }else if($scope.video){
+      $scope.showme=false;
+      $scope.tryagain=false;
+      $scope.video=!$scope.video;
+      $scope.firstname='';
+      $scope.lastname=''
+      $scope.first =true;
+      $scope.last =true;
+    }else{
+      $scope.showme=true;
+      $scope.tryagain=true;
+      $scope.first =false;
+      $scope.last =false;
+      
+    }
+  }
 });
